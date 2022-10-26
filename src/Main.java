0c9cf5531b2adb8e8a120aaed051cfb2af20dcc7
dyharlan <dyharlan45@gmail.com>
2022-10-26 22:49:53 +0800

    Cleanup deprecated code

--- src/Main.java
+++ src/Main.java
@@ -13,16 +13,9 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception{
         
-        if(args.length > 0){
-           if(args[0] == "--hrr"){
-                F1RaceSwing f1 = new F1RaceSwing(true);
-                f1.startApp();
-           }    
-        }
-        else{
             F1RaceSwing f1 = new F1RaceSwing();
             f1.startApp();
-        }
+        //}
 //        SOUNDS s = new SOUNDS();
 //        byte[] main = s.CAR_CRASH.readAllBytes();
 //        File out = new File("out.txt");
